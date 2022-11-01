fc5953f562df63467d0e653c27ca74d6980a6018
kartik1998 <kartikrawat61@gmail.com>
2022-11-01 10:06:02 +0530

    feat: add store

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Getters from './lib/getters';
 import Setters from './lib/setters';
 import Data from './lib/data';
+import Store from './lib/store';
 
-export { Getters, Setters, Data };
+export { Getters, Setters, Data, Store };
---
+++ src/lib/store.ts
@@ -0,0 +1,28 @@
+/**
+ * Sets up a storage map for all instantiated objects.
+ * Every object is added in the map as {id: object} where id is object._id.
+ * object._id is incremented whenever a new object is instantiated.
+ * Static method findById can be used to find an object by id.
+ */
+const Store =
+  () =>
+  <T extends { new (...args: any[]): {} }>(constructor: T) => {
+    let __counter = 1;
+    const storage: { [key: number]: any } = {};
+    return class extends constructor {
+      public static findById(id: number) {
+        return storage[id];
+      }
+      private _id: number;
+      getId: () => number;
+      constructor(...args: any[]) {
+        super(...args);
+        this._id = __counter++;
+        const self = this;
+        this.getId = () => self._id;
+        storage[this._id] = this;
+      }
+    };
+  };
+
+export default Store;
